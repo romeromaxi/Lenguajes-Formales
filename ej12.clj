c8e1a129ef47235ec37486fb98c9f98e6c7d56f5
Maxi Romero <mromero@itaptec.com.ar>
2021-06-24 13:25:07 -0300

    Correccion ejercicio 12

--- ej12.clj
+++ ej12.clj
@@ -2,5 +2,8 @@
 ; De lo contrario, se devolverá una lista, en la que habrá una cadena "Uno para X, uno para mí" por cada argumento X.
 (defn repartir
   ([] (println "Uno para vos, uno para mi"))
-  ([& more] (map #(printf "Uno para %s, uno para mi" %) more))
+  ([& more] (map #(str "Uno para " % ", uno para mi") more))
 )
+
+(repartir)
+(repartir 'Pablo 'Ana 'Maria)
