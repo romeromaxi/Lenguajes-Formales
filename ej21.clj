cfe989ab27e6567452c5d379ace5588215a56851
Maxi Romero <mromero@itaptec.com.ar>
2021-06-24 18:00:20 -0300

    Opcion recursiva ejercicio 21

--- ej21.clj
+++ ej21.clj
@@ -18,7 +18,22 @@
     (println "Error. La matriz no es cuadrada"))
   )
 
+(defn matriz-triangular-superior-recursivo
+    ([m]
+     (if
+      (es-matriz-cuadrada m)
+       (matriz-triangular-superior-recursivo m 0)
+       (println "Error. Matriz no cuadrada")))
+    ([m i]
+     (if
+      (empty? m) m
+      (cons (concat (repeat i 0) (first m)) (matriz-triangular-superior-recursivo (map rest (rest m)) (inc i))))
+     )
+  )
+
 (matriz-triangular-superior '((1 2 3) (4 5 6) (7 8 9)))
 (matriz-triangular-superior '((1 2 3 4 5) (6 7 8 9 10) (11 12 13 14 15) (16 17 18 19 20) (21 22 23 24 25)))
 
+(matriz-triangular-superior-recursivo '((1 2 3) (4 5 6) (7 8 9)))
+(matriz-triangular-superior-recursivo '((1 2 3 4 5) (6 7 8 9 10) (11 12 13 14 15) (16 17 18 19 20) (21 22 23 24 25)))
 
