d56d032da203d9a58488290ba1a2391339ea66e8
Maxi Romero <mromero@itaptec.com.ar>
2021-06-24 18:03:02 -0300

    Ejercicio 23

---
+++ ej23.clj
@@ -0,0 +1,6 @@
+; 23. Definir una función para transponer una lista de listas.
+(defn transponer [m]
+  (apply map list m)
+  )
+
+(transponer '((1 2 3 4 5) (6 7 8 9 10) (11 12 13 14 15) (16 17 18 19 20) (21 22 23 24 25)))
